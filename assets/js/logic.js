c64edf8f7a219a7e6ac02daeea7ae09c7873a3b4
Lynn Boudreau <lw.boudreau@live.com>
2020-05-11 15:24:42 -0400

    added buzzers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,8 +13,8 @@ var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
 // sound effects
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+var sfxRight = new Audio("assets/sound/correct buzzer");
+var sfxWrong = new Audio("assets/sound/incorrect buzzer");
 
 function startQuiz() {
   // hide start screen
